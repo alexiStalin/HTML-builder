93d7a9375546987f06a14fc6f34d27bddb3d71da
alexiStalin <lesha.stalin@gmail.com>
2021-11-10 07:22:33 +0300

    feat: add solution 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,6 +2,20 @@ const fs = require('fs');
 const path = require('path');
 
 function copyFolder() {
+   fs.readdir(
+      path.join(__dirname, 'files-copy'),
+      { withFileTypes: true },
+      (err, files) => {
+         if (err) return;
+         files.forEach((file) => {
+            fs.unlink(path.join(__dirname, 'files-copy', file.name), (err) => {
+               if (err) throw err;
+               console.log('Файл успешно delete');
+            });
+         });
+      }
+   );
+
    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
       if (err) throw err;
       console.log('Папка успешно создана');
